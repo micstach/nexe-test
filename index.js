4155b5ba537d768ec5806d80a41c704a5c7c6ca0
micstach <micstach@poczta.onet.pl>
2017-03-18 14:24:07 +0100

    commander package added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello NEXE!");
+
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-t, --token [value]', 'Set access token')
+  .option('-a, --action [value]', 'Select action', /^(push-project|pull-project|get-projects|get-hubs)$/i)
+  .option('-h, --hub [value]', 'Select hub type')
+  .parse(process.argv);
+ 
+console.log('token %s', program.token);
+
+if (typeof program.action !== 'function') 
+  console.log('action %s', program.action);
+
+console.log('hub: %s', program.hub);
